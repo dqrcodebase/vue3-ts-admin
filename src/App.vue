ec051fb7c9d56fc3c1e55560bca08372b854577a
dengqr <852601818@qq.com>
2024-02-20 17:45:06 +0800

    perf: 模块包下载限制

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ function a() {
   console.log("====================================");
 }
 console.log(a);
-var b = "";
 </script>
 
 <template>
@@ -24,12 +23,14 @@ var b = "";
 .logo {
   height: 6em;
   padding: 1.5em;
-  will-change: filter;
   transition: filter 300ms;
+  will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
