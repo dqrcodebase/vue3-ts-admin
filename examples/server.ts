340a364a0d4d29d918d2e7f3644266e10aa7ad99
dengqr <852601818@qq.com>
2024-03-18 14:15:29 +0800

    fix: 修改指令

--- examples/server.ts
+++ examples/server.ts
@@ -1,28 +1,28 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { createRoutes } from "./data.js";
+const express = require("express");
+const bodyParser = require("body-parser");
+const { createRoutes } = require("./data");
 
 const router = express.Router();
 const app = express();
-//设置跨域访问
-app.all("*", function (req, res, next) {
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin", "*");
-  //允许的header类型
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With",
-  );
-  //跨域允许的请求方式
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  // 可以带cookies
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method == "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+// //设置跨域访问
+// app.all("*", function (req, res, next) {
+//   //设置允许跨域的域名，*代表允许任意域名跨域
+//   res.header("Access-Control-Allow-Origin", "*");
+//   //允许的header类型
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Requested-With",
+//   );
+//   //跨域允许的请求方式
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//   // 可以带cookies
+//   res.header("Access-Control-Allow-Credentials", true);
+//   if (req.method == "OPTIONS") {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 registerUserRouter();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
