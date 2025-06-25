960c2912312c029c85417c06991f52ff7e95cd25
D Q R <852601818@qq.com>
2025-06-25 15:51:01 +0800

    docs: 代码注释

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,11 +3,15 @@
 /** @type {import("stylelint").Config} */
 export default {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-html/vue',
-    'stylelint-config-recess-order',
+    'stylelint-config-standard', // 基础规则
+    'stylelint-config-html/vue', // Vue 特定规则
+    'stylelint-config-recess-order', // 顺序规则
   ],
-  plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-prettier'],
+  plugins: [
+    'stylelint-scss', // SCSS 规则
+    'stylelint-order', // 顺序规则
+    'stylelint-prettier', // Prettier 集成
+  ],
   overrides: [
     {
       files: ['**/*.(css|html|vue)'],
@@ -17,26 +21,27 @@ export default {
       files: ['*.scss', '**/*.scss'],
       customSyntax: 'postcss-scss',
       extends: [
-        'stylelint-config-standard-scss',
-        'stylelint-config-recommended-vue/scss',
+        'stylelint-config-standard-scss', // SCSS 规则
+        'stylelint-config-recommended-vue/scss', // Vue 特定规则
       ],
     },
   ],
   rules: {
-    'prettier/prettier': true,
-    'selector-class-pattern': null,
-    'no-descending-specificity': null,
-    'scss/dollar-variable-pattern': null,
+    'prettier/prettier': true, // Prettier 集成规则
+    'selector-class-pattern': null, // 类选择器命名规则
+    'no-descending-specificity': null, // 禁止后代选择器
+    'scss/dollar-variable-pattern': null, // SCSS 变量命名规则
     'selector-pseudo-class-no-unknown': [
+      // 未知的伪类选择器规则
       true,
       {
-        ignorePseudoClasses: ['deep', 'global'],
+        ignorePseudoClasses: ['deep', 'global'], // 忽略未知的伪类选择器
       },
     ],
     'selector-pseudo-element-no-unknown': [
       true,
       {
-        ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'],
+        ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'], // 忽略未知的伪元素选择器
       },
     ],
     // 关闭原始规则（或你之前设置的）
@@ -61,13 +66,15 @@ export default {
       },
     ],
     'rule-empty-line-before': [
+      // 空行规则
       'always',
       {
         ignore: ['after-comment', 'first-nested'],
       },
     ],
-    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }], // 未知单位规则
     'order/order': [
+      // 顺序规则
       [
         'dollar-variables',
         'custom-properties',
